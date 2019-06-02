7cb4568000212dab47c3d1f7dafbe11d99705e66
shirley <shirley.mramirez@yahoo.com>
2019-06-01 21:42:56 -0700

    done with justify-content flex-start and flex-end

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let submit = document.querySelector('#submit');
+    let circleIcon = document.querySelector('.circleIcon');
+    let frogIcon = document.querySelector('.frogIcon');
+
+    submit.addEventListener('click', (e) => {
+        let inputValue = document.querySelector('.inputText').value;
+        
+        debugger;
+        if(inputValue === 'flex-start') {
+            circleIcon.style.position = "absolute";
+            circleIcon.style.left = "15px";
+        }
+        else if(inputValue === 'flex-end'){
+            circleIcon.style.position = "absolute";
+            circleIcon.style.top = "16px";
+            circleIcon.style.right = "31px";
+            frogIcon.style.position = "absolute";
+            frogIcon.style.right = "45px";
+        } 
+        else if(inputValue === 'center') {
+
+        } else if(inputValue === 'space-between') {
+            
+        } else if(inputValue === 'space-around'){
+
+        }
+    })
+
+
+});
